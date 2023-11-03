1ecc3360ea4a81e348eee40d2ac20da8d92255bf
Angelos Staboulis <angelosstaboulis@gmail.com>
2023-11-03 15:03:22 +0200

    Initial Commit

---
+++ SwiftCalendar/Helper/Helper.swift
@@ -0,0 +1,49 @@
+//
+//  Helper.swift
+//  SwiftCalendar
+//
+//  Created by Angelos Staboulis on 3/11/23.
+//
+
+import Foundation
+class Helper{
+    static let shared = Helper()
+    private init(){}
+    func getDate() -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        return formatter.string(from: Date())
+    }
+    func getTime() -> String{
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "hh:mm:ss"
+        return formatter.string(from: Date())
+    }
+    func getTimeZone()->String{
+        let formatter = DateFormatter()
+        return formatter.timeZone.identifier
+    }
+    func getNameDay() async -> String{
+        return await withCheckedContinuation { checked in
+            Task.init{
+                let nameDay = await NameDayViewModel.shared.getNameDayModel().nameday
+                let getNameDay = nameDay["nameday"]
+                let nameDayArray = nameDay["us"]!.components(separatedBy: ",")
+                if nameDayArray.count > 1 {
+                    var newArray:String!=""
+                    for item in 0..<nameDayArray.count{
+                        newArray = newArray + nameDayArray[item]
+                    }
+                    checked.resume(returning:newArray)
+                }else{
+                    checked.resume(returning:nameDayArray[0])
+
+                }
+            }
+        }
+        
+        
+        
+    }
+}
---
+++ SwiftCalendar/views/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  SwiftCalendar
+//
+//  Created by Angelos Staboulis on 3/11/23.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+class ViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var lblTimeZone: UILabel!
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var lblDate: UILabel!
+    @IBOutlet weak var lblNameDay: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createView()
+   
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.contentSize = CGSize(width: 2000, height: 0)
+    }
+
+}
+extension ViewController{
+    func createView(){
+        Task.init{
+            lblDate.text = Helper.shared.getDate()
+            lblTime.text = Helper.shared.getTime()
+            lblTimeZone.text =  Helper.shared.getTimeZone()
+            lblNameDay.text = await Helper.shared.getNameDay()
+        }
+  
+
+    }
+}
